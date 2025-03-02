dd208a0aa0408b85a78e9fc021485e4e7e5ee747
Om Nepal <nepalom15@gmail.com>
2025-03-02 12:38:57 -0600

    display error if required entry fields are empty

--- scripts.js
+++ scripts.js
@@ -3,26 +3,59 @@ const workoutNameElement = document.querySelector('.js-workout-name')
 const numberOfSetsElement = document.querySelector('.js-number-of-sets')
 const workoutDate = document.querySelector('.js-workout-date')
 const workoutNotes = document.querySelector('.js-workout-notes')
+const workoutTableInitialRow = document.querySelector('.js-workout-table-inital-row')
 const workoutTableBodyElement = document.querySelector('.js-workout-table-body')
 
+const errorMessageElement = document.querySelector('.js-error-message')
+
+const requiredEntriesArray = document.querySelectorAll('.js-add-workout-entry')
+
 let workoutDataArray = []
+let isEmpty;
 
 addWorkoutButton.addEventListener('click', () => {
 
-  const workoutDataArray = createWorkoutDataArray();
-  console.log(workoutDataArray)
+  isEmpty = checkForEmptyFields()
 
-  workoutTableBodyElement.innerHTML += `
-  <td>${workoutNameElement.value}</td>
-  <td>${numberOfSetsElement.value}</td>
-  <td>${workoutDate.value}</td>
-  <td class="table-paragraph">
-    ${workoutNotes.value}
-  </td>
-  `
-  clearEntryFields()
+  if (!isEmpty) {
+    const workoutDataArray = createWorkoutDataArray();
+    console.log(workoutDataArray)
+    workoutTableInitialRow.innerHTML = ''
+
+    workoutTableBodyElement.innerHTML += `
+    <td>${workoutNameElement.value}</td>
+    <td>${numberOfSetsElement.value}</td>
+    <td>${workoutDate.value}</td>
+    <td class="table-paragraph">
+      ${workoutNotes.value}
+    </td>
+    `
+    clearEntryFields()
+  }
+
+  
 
   })
+  
+function checkForEmptyFields() {
+  if (workoutNameElement.value === '' || numberOfSetsElement.value === '' || workoutDate.value === '') {
+    
+    requiredEntriesArray.forEach((requiredEntry) => {
+      requiredEntry.classList.add('workout-entry-error')
+    })
+    errorMessageElement.innerHTML = '**Complete all required fields**'
+
+    return true;
+
+  } else if (workoutNameElement.value !== '' && numberOfSetsElement.value !== '' && workoutDate.value !== '') {
+    requiredEntriesArray.forEach((requiredEntry) => {
+      requiredEntry.classList.remove('workout-entry-error')
+    })
+    errorMessageElement.innerHTML = ''
+
+    return false
+  }
+}
   
 function createWorkoutDataArray() {
   workoutDataArray.push({
